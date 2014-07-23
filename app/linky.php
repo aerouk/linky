22fa32eb5f9a50c4ee001035a4180139707a11c6
Tom B <aerouk@users.noreply.github.com>
2014-07-24 00:36:37 +0100

    OO mysql for application

--- app/linky.php
+++ app/linky.php
@@ -19,16 +19,16 @@ if ($_GET['action'] == "redirect") {
 
     $uri = $_GET['uri'];
 
-    if ($statement = mysqli_prepare($connection, "SELECT url FROM linky_urls WHERE uri = ?")) {
-        mysqli_stmt_bind_param($statement, "s", $uri);
-        mysqli_stmt_execute($statement);
-        mysqli_stmt_bind_result($statement, $url);
-        mysqli_stmt_fetch($statement);
-        mysqli_stmt_close($statement);
+    if ($statement = $connection->prepare("SELECT url FROM linky_urls WHERE uri = ?")) {
+        $statement->bind_param("s", $uri);
+        $statement->execute();
+        $statement->bind_result($url);
+        $statement->fetch();
+        $statement->close();
         redirect($url != "" ? $url : APPLICATION_URL . "?e=unf");
     }
 
-    mysqli_close($connection);
+    $connection->close();
     die();
 } elseif ($_GET['action'] == "create") {
     if ( ! isset($_POST['url']) || $_POST['url'] == "") {
@@ -38,15 +38,15 @@ if ($_GET['action'] == "redirect") {
     $url = preg_match("@^https?://@", $_POST['url']) ? $_POST['url'] : "http://" . $_POST['url'];
     $uri = (isset($_POST['uri']) && $_POST['uri'] != "") ? $_POST['uri'] : generateRandomURI();
 
-    if ($statement = mysqli_prepare($connection, "INSERT INTO linky_urls (url,uri) VALUES (?, ?)")) {
-        mysqli_stmt_bind_param($statement, "ss", $url, $uri);
-        mysqli_stmt_execute($statement);
-        mysqli_stmt_fetch($statement);
-        mysqli_stmt_close($statement);
+    if ($statement = $connection->prepare("INSERT INTO linky_urls (url,uri) VALUES (?, ?)")) {
+        $statement->bind_param("ss", $url, $uri);
+        $statement->execute();
+        $statement->fetch();
+        $statement->close();
         redirect(APPLICATION_URL . "?url=$url&uri=$uri");
     }
 
-    mysqli_close($connection);
+    $connection->close();
     die();
 } else {
     redirect(APPLICATION_URL . "?e=nq");
