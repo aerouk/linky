c859b418f0af7a6b9d42f6270b5b53a8e788e1de
Tom B <me@iaero.me>
2016-10-03 10:11:22 +0100

    Config option for custom URI

--- app/config.php
+++ app/config.php
@@ -8,6 +8,8 @@
 define('APPLICATION_NAME', "linky"); # Shown in page titles
 define('APPLICATION_URL', ""); # Include a trailing slash at the end. E.g. http://google.com/
 
+define('ALLOW_CUSTOM_URI', true); # Allow custom URI
+
 define('DATABASE_HOST', ""); # Database hostname/ip address
 define('DATABASE_USER', ""); # Database username
 define('DATABASE_PASS', ""); # Database password
